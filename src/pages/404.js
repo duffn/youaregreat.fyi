35af5ec12ebea867b2919a4a73f30ff9f77430bd
Nicholas Duffy <3457341+duffn@users.noreply.github.com>
2023-01-03 12:51:34 -0700

    Upgrade to Gatsby 5 and Go 1.16

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,10 @@
 import React from "react";
-import { Helmet } from "react-helmet";
 import { Link } from "gatsby";
 import "../styles/index.css";
 
 function NotFound() {
   return (
     <main>
-      <Helmet>
-        <title>404</title>
-      </Helmet>
       <h1>Page Not Found.</h1>
       <p>You are still great.</p>
       <p>
@@ -19,3 +15,7 @@ function NotFound() {
 }
 
 export default NotFound;
+
+export function Head() {
+  return <title>404</title>;
+}
